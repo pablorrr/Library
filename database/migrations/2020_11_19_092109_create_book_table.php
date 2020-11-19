5fd1b646063af688834f9917106757554a8cabef
Paweł Kalisz <pawel.kalisz@inspirelabs.pl>
2020-11-19 10:48:18 +0100

    add model and migrations files

---
+++ database/migrations/2020_11_19_092109_create_book_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBookTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('book', function (Blueprint $table) {
+            $table->bigIncrements('id_book');
+            $table->string('title', 32)->unique();
+            $table->string('author', 32)->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {   Schema::disableForeignKeyConstraints();
+        Schema::dropIfExists('book');
+        Schema::enableForeignKeyConstraints();
+    }
+}
---
+++ database/migrations/2020_11_19_092244_create_turned_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTurnedTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('turned', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('id_borrower')->unsigned();
+            $table->integer('id_borrowed')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::disableForeignKeyConstraints();
+        Schema::dropIfExists('turned');
+        Schema::enableForeignKeyConstraints();
+    }
+}

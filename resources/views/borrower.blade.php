376b2b17deb8cb3b98c38b405de6976d7b8c5284
Paweł Kalisz <pawel.kalisz@inspirelabs.pl>
2020-12-25 16:57:43 +0100

    prepare tables and views

--- resources/views/borrower.blade.php
+++ resources/views/borrower.blade.php
@@ -7,53 +7,93 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <style>
+        /* Remove the navbar's default margin-bottom and rounded borders */
+        .navbar {
+            margin-bottom: 0;
+            border-radius: 0;
+        }
+
+        /* Add a gray background color and some padding to the footer */
+        footer {
+            background-color: #f2f2f2;
+            padding: 25px;
+        }
+    </style>
 </head>
 <body>
 
-<div class="jumbotron text-center">
-    <h1>Borrower (User)</h1>
-    <p><a href='{!! url('/'); !!}'>Main Page</a></p>
-    <p><a href='{!! url('/turned'); !!}'>Turned</a></p>
-    <p><a href='{!! url('/borrower'); !!}'>Borrower</a></p>
-    <p><a href='{!! url('/borrowed'); !!}'>Borrowed</a></p>
+<nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Book</a>
+        </div>
+        <div class="collapse navbar-collapse" id="myNavbar">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="{!! url('/'); !!}">Book</a></li>
+                <li><a href="{!! url('/turned'); !!}">Turned</a></li>
+                <li><a href="{!! url('/borrower'); !!}">Borrower</a></li>
+                <li><a href="{!! url('/borrowed'); !!}">Borrowed</a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<div class="jumbotron">
+    <div class="container text-center">
+        <h1>Book</h1>
+        <p>Main page</p>
+    </div>
 </div>
 
-<div class="container">
+<div class="container-fluid bg-3 text-center">
+
     <div class="row">
         <table class="table">
             <thead class="thead-dark">
             <tr>
-                <th scope="col">#</th>
-                <th scope="col">First</th>
-                <th scope="col">Last</th>
-                <th scope="col">Handle</th>
+                <th scope="col">id</th>
+                <th scope="col">Imie</th>
+                <th scope="col">Nazwisko</th>
+
             </tr>
             </thead>
             <tbody>
             <tr>
                 <th scope="row">1</th>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>@mdo</td>
+                <td></td>
+                <td></td>
+
             </tr>
             <tr>
                 <th scope="row">2</th>
-                <td>Jacob</td>
-                <td>Thornton</td>
-                <td>@fat</td>
+                <td></td>
+                <td></td>
+
             </tr>
             <tr>
                 <th scope="row">3</th>
-                <td>Larry</td>
-                <td>the Bird</td>
-                <td>@twitter</td>
+                <td></td>
+                <td></td>
+
             </tr>
             </tbody>
         </table>
-
-
     </div>
-</div>
+    <br>
+
+
+    <footer class="container-fluid text-center">
+        <p>Footer Text</p>
+    </footer>
 
 </body>
 </html>
